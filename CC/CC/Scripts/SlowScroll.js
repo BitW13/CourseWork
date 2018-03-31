3fc66bbb1666e546937949f369149d1a624b1481
YStoukachev <ystoukachev@gmail.com>
2018-04-01 01:43:10 +0300

    Few features of disign

--- CC/CC/Scripts/SlowScroll.js
+++ CC/CC/Scripts/SlowScroll.js
@@ -1,7 +1,8 @@
 ﻿//id - того блока до которого надо прокрутить 
+//<li><a href="javascript://0" onclick="slowScroll('#sk1')">ABOUT</a></li>
 
 function slowScroll(id) {
-    var offset = 40;
+    var offset = 50;
     $('html, body').animate({
         scrollTop: $(id).offset().top - offset
     }, 1000);
